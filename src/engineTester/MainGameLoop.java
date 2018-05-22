562bea291a5629b942f63f457292b6365a2472d7
dolatapatryk1 <dolata.patryk1@gmail.com>
2018-05-23 00:00:30 +0200

    first texture

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,11 +2,13 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -30,12 +32,21 @@ public class MainGameLoop {
 				3,1,2   //Bottom right triangle v3, v1, v2
 		};
 		
-		RawModel model = loader.loadToVao(vertices, indices);
+		float[] textureCoords = {
+				0,0, //V0
+				0,1, //V1
+				1,1, //V2
+				1,0 //V3
+		};
+		
+		RawModel model = loader.loadToVao(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("texture"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
 		while(!Display.isCloseRequested()) {
 			renderer.prepare(); 
 			shader.start();
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
